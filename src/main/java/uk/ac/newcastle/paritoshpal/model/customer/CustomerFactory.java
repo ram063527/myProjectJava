7b5104ad53ad81a3098d68d1f32b21c28f40b9b1
Ram Lakhan <ram063527@gmail.com>
2025-10-07 20:50:00 +0100

    Java Docs for Customer Factory

--- src/main/java/uk/ac/newcastle/paritoshpal/model/customer/CustomerFactory.java
+++ src/main/java/uk/ac/newcastle/paritoshpal/model/customer/CustomerFactory.java
@@ -3,14 +3,34 @@ package main.java.uk.ac.newcastle.paritoshpal.model.customer;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * A factory for creating and managing unique {@link Customer} instances.
+ * This class ensures that for any given name, only one {@code Customer}
+ * object exists within the application.
+ *
+ */
 public final class CustomerFactory {
+
 
     private static final Map<Name, Customer> customers = new HashMap<>();
 
+    /**
+     * Private constructor to prevent instantiation of this utility class.
+     */
     private CustomerFactory() {
-        // Empty
+        // Empty by design.
     }
 
+    /**
+     * Gets the unique {@link Customer} instance for the given name.
+     * If a customer with the specified first and last name already exists,
+     * the existing instance is returned. Otherwise, a new {@code Customer}
+     * is created, stored for future requests, and then returned.
+     * @param firstName the first name of the customer; cannot be null or empty.
+     * @param lastName the last name of the customer; cannot be null or empty.
+     * @return the unique, non-null {@code Customer} instance.
+     * @throws IllegalArgumentException if either name component is invalid.
+     */
     public static Customer getCustomer(String firstName, String lastName) {
 
         Name nameToFind = new Name(firstName, lastName);
