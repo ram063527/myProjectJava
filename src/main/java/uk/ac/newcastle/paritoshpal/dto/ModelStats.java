dbf2d6a0e9ea62e37eb0a233905904acbd09e3bc
Ram Lakhan <ram063527@gmail.com>
2025-10-07 15:24:58 +0100

    Java docs for Dto Records - ModelStats and PartsStats

--- src/main/java/uk/ac/newcastle/paritoshpal/dto/ModelStats.java
+++ src/main/java/uk/ac/newcastle/paritoshpal/dto/ModelStats.java
@@ -1,7 +1,12 @@
 package main.java.uk.ac.newcastle.paritoshpal.dto;
 
-import main.java.uk.ac.newcastle.paritoshpal.model.pc.PCModel;
+
 import main.java.uk.ac.newcastle.paritoshpal.model.pc.PresetModel;
 
+/**
+ * A data transfer object to hold statistics about the most ordered models.
+ * @param model
+ * @param modelCount
+ */
 public record ModelStats(PresetModel model, int modelCount) {
 }
--- src/main/java/uk/ac/newcastle/paritoshpal/dto/PartsStats.java
+++ src/main/java/uk/ac/newcastle/paritoshpal/dto/PartsStats.java
@@ -1,4 +1,9 @@
 package main.java.uk.ac.newcastle.paritoshpal.dto;
 
+/**
+ * A data transfer object to hold statistics about the most ordered parts
+ * @param parts
+ * @param partCount
+ */
 public record PartsStats(String parts, int partCount) {
 }
