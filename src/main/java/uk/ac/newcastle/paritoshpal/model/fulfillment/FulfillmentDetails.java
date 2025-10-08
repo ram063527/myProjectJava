b2f212fa3482b6a9dc9f1064104cb8bb287b602e
Ram Lakhan <ram063527@gmail.com>
2025-10-08 11:15:07 +0100

    Java Docs and immutability for the fulfillment details class

--- src/main/java/uk/ac/newcastle/paritoshpal/model/fulfillment/FulfillmentDetails.java
+++ src/main/java/uk/ac/newcastle/paritoshpal/model/fulfillment/FulfillmentDetails.java
@@ -3,16 +3,35 @@ package main.java.uk.ac.newcastle.paritoshpal.model.fulfillment;
 import java.util.Map;
 import java.util.Objects;
 
+/**
+ * An immutable data class that holds the aggregated details required to
+ * fulfill an order.
+ * The object is returned by the {@code fullfillOrder} method and contains
+ * two main pieces of information: a breakdown of preset models to be ordered
+ * from manufacturers, list of individual parts to be collected from the warehouse
+ * for custom models.
+ */
 public final class FulfillmentDetails {
 
     private final Map<String, Map<String,Integer>> presetOrders;
     private final Map<String,Integer> warehouseParts;
 
+    /**
+     * Constructs a new FulfillmentDetails instance.
+     * @param presetOrders A map where the key is the manufacturer's name and the value
+     * is another map, holding preset model names and their required quantities.
+     * @param warehouseParts A map where the key is the name of a part for a custom
+     * model and the value is its required quantity.
+     */
     public FulfillmentDetails(Map<String, Map<String, Integer>> presetOrders, Map<String, Integer> warehouseParts) {
-        this.presetOrders = presetOrders;
-        this.warehouseParts = warehouseParts;
+        this.presetOrders = Map.copyOf(presetOrders);
+        this.warehouseParts = Map.copyOf(warehouseParts);
     }
 
+    /**
+     * Returns a string representation of the fulfillment details.
+     * @return a formatted string showing the preset orders and warehouse parts.
+     */
     @Override
     public String toString() {
         return "FulfillmentDetails{" +
@@ -21,6 +40,12 @@ public final class FulfillmentDetails {
                 '}';
     }
 
+    /**
+     * Compares this Fulfillment object to another for equality
+     * @param o the object to compare against.
+     * @return {@code true} if both objects have identical preset order maps
+     * and warehouse part maps; {@code false} otherwise.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
