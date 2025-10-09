cde6fd1302003390e91f4b55cd723d0097f8303d
Ram Lakhan <ram063527@gmail.com>
2025-10-09 07:16:30 +0100

    package refactoring

---
+++ src/test/uk/ac/newcastle/paritoshpal/model/customer/NameTest.java
@@ -0,0 +1,68 @@
+package test.java.uk.ac.newcastle.paritoshpal.model.customer;
+
+import main.java.uk.ac.newcastle.paritoshpal.model.customer.Name;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NameTest {
+
+    @Test
+    void testNameCreation(){
+        // Setup
+        String firstName = "Paritosh";
+        String lastName = "Pal";
+
+        // Execution
+        Name name = new Name(firstName,lastName);
+
+        // verification
+        //  Normal Case
+        assertEquals("paritosh",name.getFirstName());
+        assertEquals("pal",name.getLastName());
+
+        // null case
+        String firstName2 = null;
+        String lastName2 = "Pal";
+        assertThrowsExactly(IllegalArgumentException.class,()->{
+            Name name2 = new Name(firstName2,lastName2);
+        });
+
+        // invalid character
+        String firstName3 = "Paritosh@12";
+        String lastName3 = "pal23";
+        assertThrowsExactly(IllegalArgumentException.class,()->{
+            Name name3 = new Name(firstName3,lastName3);
+        });
+
+        // normalization
+        String firstName4 = "Paritosh    ";
+        String lastName4 = "      Pal    ";
+        assertEquals("paritosh",name.getFirstName());
+        assertEquals("pal",name.getLastName());
+    }
+
+    @Test
+    void getFirstName() {
+    }
+
+    @Test
+    void getLastName() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void valueOf() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+}
